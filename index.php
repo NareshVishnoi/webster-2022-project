af8a800a91bdedba13a1a815f5876915d1658abd
naresh-xyz <97459274+naresh-xyz@users.noreply.github.com>
2022-10-29 17:39:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,20 +7,20 @@ $username="root";
 $password="";
 $database="login";
 $con=mysqli_connect($server,$username,$password,$database);
-if(isset($_POST['naresh'])){
+if(isset($_POST['name']) && isset($_POST['password'])){
     
     
    
      $name=$_POST['name'];
-     $email=$_POST['email'];
-     $query="select * from login where name='$name' and email='$email'";
+     $password=$_POST['password'];
+     $query="select * from login where name='$name' and password='$password'";
      $result=mysqli_query($con,$query);
      $rows=mysqli_num_rows($result);
      if($rows>0){
         
         session_start();
         $_SESSION['name']=$name;
-        $_SESSION['email']=$email;
+        $_SESSION['password']=$password;
     header("Location:login.php");
      }
      else{
@@ -37,29 +37,38 @@ if(isset($_POST['naresh'])){
     </head>
     <body>
         <div class="head">
-            <h1>Login</h1>
         </div>
         <div class="main">
             <!-- this is main content -->
-            <div class="wrapper">
+            <div class="wrapper"> 
+            <a href="login.php"><img src="https://see.fontimg.com/api/renderfont4/VGORe/eyJyIjoiZnMiLCJoIjozMywidyI6MTUwMCwiZnMiOjIyLCJmZ2MiOiIjRkZGRUZFIiwiYmdjIjoiI0ZGRkZGRiIsInQiOjF9/U01JVC1FLVNIT1A/minecraft-ten.png" alt="Minecraft fonts"></a>
                 <form action="index.php" method="post">
-                    <input type="text" name="name" id="name" placeholder="name">
-                    <input type="email" name="email" id="email" placeholder="Email">
+                    <input class="input1" type="text" class="input" name="name"  id="name" placeholder="Name">
+                    <input class ="input2" type="password" class="input" name="password" id="password" placeholder="Password">
                     <br>
-                   <button name="naresh" value="Login" class="btn">Login</button>
-                   <button class="btn"><a href="Sign-up.php">New-user</a></button>
+                    <br>
+                    <br>
+                   <button  value="Login" class="btn1">Login</button>
+                   <p>or</p>
+                   <button class="btn2"><a href="Sign-up.php">Create an account</a></button>
                 </form>
                 <?php
                 if($insert==false){
-                    echo "Please check you credentials";
+                    echo "Incorrect details entered";
                 }
            ?>
             </div>
-           
+           <div class="chat">
+            <p class="para">Trouble logging in</p>
+            <a href="web.whatsapp">
+                <img src="chat.png" alt="" class="img1">
+            </a>
+            <!-- <img class="img1" src="chat.png" alt=""> -->
+           </div>
         </div>
         <div class="footer">
             <!-- here goes our footer  -->
-            All rights reserved under section 1990 Act, 343041,jalore,rajasthan(India)
+           <p class="p1"> © 2022 Team Learner,Inc. All rights reserved</p>
         </div>
     </body>
 </html>
